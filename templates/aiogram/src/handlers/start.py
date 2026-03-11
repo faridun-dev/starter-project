519b565b49c32d818db515701e014ad050a84a70
Faridun Fatkhullozoda <faridunf200630@gmail.com>
2026-03-11 14:54:06 +0500

    feat: added aiogram template

---
+++ templates/aiogram/src/handlers/start.py
@@ -0,0 +1,10 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def start_handler(message: Message):
+    await message.answer("Hello! 👋 I'm your bot.")
