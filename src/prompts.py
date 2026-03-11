99c2ae4c0ef35894d00f1cead21469197f98f223
Faridun Fatkhullozoda <faridunf200630@gmail.com>
2026-03-11 11:03:35 +0500

    feat: impl base prompting

---
+++ src/prompts.py
@@ -0,0 +1,14 @@
+import questionary
+
+
+def ask_questions():
+    name = questionary.text("Project name:").ask()
+
+    template = questionary.select(
+        "Choose template:",
+        choices=["FastAPI", "Flask API", "CLI Tool", "Telegram Bot"],
+    ).ask()
+
+    git = questionary.confirm("Initialize git?").ask()
+
+    return {"name": name, "template": template, "git": git}
