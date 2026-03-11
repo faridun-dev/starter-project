81f7bd73d7b61b0574a478c037a9bc11aeef8301
Faridun Fatkhullozoda <faridunf200630@gmail.com>
2026-03-11 11:03:43 +0500

    feat: impl base cli

---
+++ src/cli.py
@@ -0,0 +1,15 @@
+import typer
+from src.prompts import ask_questions
+from src.generator import create_project
+
+app = typer.Typer()
+
+
+@app.command()
+def create():
+    answers = ask_questions()
+    create_project(answers)
+
+
+if __name__ == "__main__":
+    app()
